f8eb6919d14c74672c983cc840d1efa5f554d240
Mike Lockwitz <mlockwitz@gmail.com>
2018-11-14 15:16:31 -0700

    Adding results and updating api to process github request.

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,5 +1,33 @@
 var React = require('react');
 var PropTypes = require('prop-types');
+var Link = require('react-router-dom').Link;
+
+function PlayerPreview(props) {
+    return (
+        <div>
+            <div className='column'>
+                <img
+                    className='avatar'
+                    src={props.avatar}
+                    alt={'Avatar for ' + props.username}
+                />
+                <h2 className='username'>@{props.username}</h2>
+            </div>
+            <button
+                className='reset'
+                onClick={props.onReset.bind(null, props.id)}>
+                Reset
+            </button>
+        </div>
+    )
+}
+
+PlayerPreview.propTypes = {
+    avatar: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+    onReset: PropTypes.func.isRequired,
+    id: PropTypes.string.isRequired,
+}
 
 class PlayerInput extends React.Component {
     constructor(props) {
@@ -77,6 +105,7 @@ class Battle extends React.Component {
         }
 
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleReset = this.handleReset.bind(this);
     }
 
     handleSubmit(id, username) {
@@ -89,9 +118,22 @@ class Battle extends React.Component {
         })
     }
 
+    handleReset(id) {
+        this.setState(function() {
+            var newState = {};
+            newState[id + 'Name'] = '';
+            newState[id + 'Image'] = null;
+
+            return newState;
+        })
+    }
+
     render() {
+        var match = this.props.match;
         var playerOneName = this.state.playerOneName;
         var playerTwoName = this.state.playerTwoName;
+        var playerOneImage = this.state.playerOneImage;
+        var playerTwoImage = this.state.playerTwoImage;
 
         return (
             <div>
@@ -103,6 +145,15 @@ class Battle extends React.Component {
                             onSubmit = {this.handleSubmit}
                         />
                     }
+                    {playerOneImage !== null &&
+                        <PlayerPreview
+                            id='playerOne'
+                            username={playerOneName}
+                            avatar={playerOneImage}
+                            onReset = {this.handleReset}
+                        />
+                    }
+
                     {!playerTwoName &&
                         <PlayerInput
                             id='playerTwo'
@@ -110,7 +161,27 @@ class Battle extends React.Component {
                             onSubmit={this.handleSubmit}
                         />
                     }
+                    {playerTwoImage !== null &&
+                        <PlayerPreview
+                            id='playerTwo'
+                            username={playerTwoName}
+                            avatar={playerTwoImage}
+                            onReset={this.handleReset}
+                        />
+                    }
                 </div>
+
+                {playerOneImage && playerTwoImage &&
+                    <Link
+                        className='button'
+                        to={{
+                            pathname: match.url + '/results',
+                            search: `?playerOneName=` + playerOneName + `&playerTwoName=` + playerTwoName
+                        }}
+                    >
+                        Battle
+                    </Link>
+                }
             </div>
         )
     }
--- app/utils/api.js
+++ app/utils/api.js
@@ -1,6 +1,67 @@
 var axios = require('axios');
 
+var id = 'e3eb41f5e35dad679b8b';
+var sec = 'a5bea461e73747cba4190b9e287be77fe8698645';
+var params = '?client_id=' + id + '&client_secret=' + sec;
+
+function getProfile(username) {
+    return axios.get('https://api.github.com/users/' + username + params)
+        .then(function(user) {
+            return user.data;
+        });
+}
+
+function getRepos(username) {
+    return axios.get('https://api.github.com/users/' + username + '/repos' + params + '&per_page=100')
+}
+
+function getStarCount(repos) {
+    return repos.data.reduce(function(count, repo) {
+        return count + repo.stargazers_count;
+    }, 0)
+}
+
+function calculateScore(profile, repos) {
+    var followers = profile.followers;
+    var totalStars = getStarCount(repos);
+
+    return (followers * 3) + totalStars;
+}
+
+function handleError(error) {
+    console.warn(error);
+    return null;
+}
+
+function getUserData(player) {
+    return axios.all([
+        getProfile(player),
+        getRepos(player)
+    ]).then(function(data) {
+        var profile = data[0];
+        var repos = data[1];
+
+        return {
+            profile: profile,
+            score: calculateScore(profile, repos)
+        }
+    })
+}
+
+function sortPlayers(players) {
+    // returns array with highest score first
+    return players.sort(function(a,b) {
+        return b.score = a.score;
+    })
+}
+
 module.exports = {
+    battle: function(players) {
+        return axios.all(players.map(getUserData))
+            .then(sortPlayers)
+            .catch(handleError)
+    },
+
     fetchPopularRepos: function(language) {
         var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:' + language + '&sort=stars&order=desc&type=Repositories');
 
